ab43da371facd72fff9d6dae11313bdf3dfb41f2
amysia <amysia@knu.ac.kr>
2024-01-31 15:08:32 +0900

    [BE] feat: 카드 조회 기능 구현 (#15)
    - 변경 사항 : public 빠진 메서드 붙이기

--- DEVoca-api/src/main/java/com/ssafy/devoca/card/service/CardService.java
+++ DEVoca-api/src/main/java/com/ssafy/devoca/card/service/CardService.java
@@ -5,9 +5,9 @@ import com.ssafy.devoca.card.model.CardDTO;
 import java.util.List;
 
 public interface CardService {
-    void registerCard(CardDTO cardDTO) throws Exception;
+    public void registerCard(CardDTO cardDTO) throws Exception;
 
-    CardDTO getCardDetail(int cardId) throws Exception;
+    public CardDTO getCardDetail(int cardId) throws Exception;
 
-    List<CardDTO> getCardList(int scroll, String userId) throws Exception;
+    public List<CardDTO> getCardList(int scroll, String userId) throws Exception;
 }
