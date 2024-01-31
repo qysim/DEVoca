206d27b64146be4c0259a4a07e4d2f1125fee4bf
qysim <qysim@naver.com>
2024-01-31 17:13:15 +0900

    [FE] feat: 상단바, 하단바 작업 중 (#22)
    - 상단바 로고 스타일 논의 중
    - 하단바 작업 중

--- DEVoca-frontend/src/App.vue
+++ DEVoca-frontend/src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="container mx-auto" >
-
-    <RouterView/>
-      
+  <div class="container mx-auto">
+    <TopNavBarComponent />
+    <RouterView />
+    <BottomNavbarComponent />
   </div>
 </template>
 
 <script setup>
+import TopNavBarComponent from '@/components/TopNavbarComponent.vue'
+import BottomNavbarComponent from './components/BottomNavbarComponent.vue'
 
 </script>
 
 <style scoped>
-.devoca-bg-color {
-  background-color: rgb(0, 180, 216);
-}
 
 </style>
--- DEVoca-frontend/tailwind.config.js
+++ DEVoca-frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jalnan: ['JalnanGothic'],
+      },
+      colors: {
+        devoca: '#00B4D8'
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
