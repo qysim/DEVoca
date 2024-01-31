677380dd9628e6712f80912677a682c091e39e85
se03013 <se03013@naver.com>
2024-01-31 16:35:20 +0900

    [FE] feat: 관심분야 설정 페이지 구현(#25)
    관심분야 페이지 구현

--- DEVoca-frontend/src/router/index.js
+++ DEVoca-frontend/src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 
+import SignupView from '@/views/SignupView.vue'
+import SelectInterestsView from '@/views/SelectInterestsView.vue'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -19,6 +23,11 @@ const router = createRouter({
       name : 'LoginView',
       component : LoginView
     },
+    {
+      path: '/selectinterests',
+      name: 'SelectInterestsView',
+      component : SelectInterestsView
+    }
   ]
 })
 
