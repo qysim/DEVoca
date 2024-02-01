901bf3cdfefa2f7c306df69e5b1e70fec377194d
se03013 <se03013@naver.com>
2024-02-01 21:49:48 +0900

    [FE] feat : 팔로우 & 팔로워 화면 추가 (#36)
    팔로우 & 팔로워 화면 구현

---
+++ DEVoca-frontend/src/views/Mypage/FollowerView.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex flex-row">
+        <a href=""><img src="@/assets/images/exit.png" alt="" class="mt-5 ml-5"></a>
+        <p class="mt-6 ml-5 font-[GmarketSansMedium] text-xl">여기는 팔로잉</p>
+    </div>
+    <div>
+        <FollowerComponents/>
+    </div>
+
+</template>
+
+
+<script setup>
+import FollowerComponents from '@/components/mypage/FollowerComponents.vue';
+
+
+</script>
+
+<style scoped>
+
+</style>
