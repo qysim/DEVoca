c0c0194755ca13f8659c718e849262803236d8e7
gysim <qysim@naver.com>
2024-02-03 22:27:10 +0900

    [FE] feat: 카드 작성 페이지 구현 (#47)
    - favicon.ico 변경
    - index.js router 구조 변경
    - MainView 와 Feed 관련 구조 변경
    - 카드 생성 관련 view, component 생성

---
+++ DEVoca-frontend/src/views/feed/FeedListView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <!-- 테스트용 코드 추후 변경 예정 -->
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <WriteButtonComponent onclick="bottom_modal.showModal()"/>
+    <BottomModalComponent />
+  </div>
+</template>
+
+<script setup>
+import CardComponent from '@/components/feed/CardComponent.vue'
+import WriteButtonComponent from '@/components/feed/WriteButtonComponent.vue'
+import BottomModalComponent from '@/components/common/BottomModalComponent.vue'
+
+</script>
+
+<style scoped>
+
+</style>
