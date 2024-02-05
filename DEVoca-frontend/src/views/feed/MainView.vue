59103f11bc9c2697d8d13afcff129edd4af7a06b
gysim <qysim@naver.com>
2024-02-05 23:41:00 +0900

    [FE] feat: 검색 결과 페이지 생성 (#75)
    - SearchResultView.vue 생성

--- DEVoca-frontend/src/views/feed/MainView.vue
+++ DEVoca-frontend/src/views/feed/MainView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="py-16">
     <TopNavbarComponent/>
-    <RouterView/>
+    <RouterView />
     <BottomNavbarComponent />
   </div>
 </template>
---
+++ DEVoca-frontend/src/views/search/SearchResultView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="m-2">
+    <SearchBarComponent />
+
+    <div role="tablist" class="tabs tabs-bordered tabs-lg">
+      <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="글" />
+      <div role="tabpanel" class="tab-content p-10">내용</div>
+      
+      <input type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="단어" />
+      <div role="tabpanel" class="tab-content p-10">내용</div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SearchBarComponent from '@/components/common/SearchBarComponent.vue'
+
+</script>
+
+<style scoped>
+
+</style>
