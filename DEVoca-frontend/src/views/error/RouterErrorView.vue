a0954ceeb4b4f2349583a822c2c432df6443499c
qysim <qysim@naver.com>
2024-02-06 16:29:48 +0900

    [FE] feat: 존재하지 않는 경로 접속 처리 (#82)
    - RouterErrorView.vue 생성
    - 정의된 router path 이외의 경로는 RouterErrorView로 이동

---
+++ DEVoca-frontend/src/views/error/RouterErrorView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col justify-center items-center h-screen text-2xl font-jalnan">
+    <p>잘못된 경로입니다</p>
+    <p>메인 페이지로 이동합니다</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
