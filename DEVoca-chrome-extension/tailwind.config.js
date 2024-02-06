21fcc00565acf813dcc5c53e8ed76f11a8c47196
cheesecat47 <cheesecat47@gmail.com>
2024-02-06 07:31:21 +0000

    [FE] feat: 로고, 폰트 추가 (#72)

--- DEVoca-chrome-extension/tailwind.config.js
+++ DEVoca-chrome-extension/tailwind.config.js
@@ -1,9 +1,24 @@
+import daisyui from 'daisyui'
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./**/*.{html,js}"],
+  content: ['./**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['GmarketSansMedium', ...defaultTheme.fontFamily.sans],
+        jalnan: ['JalnanGothic']
+      },
+      colors: {
+        devoca: '#00B4D8', // 대표색
+        devoca_sky: '#DDF7FC',
+        devoca_skyblue: '#ECFCFF' // 연하늘
+      }
+    }
   },
-  plugins: [],
+  plugins: [
+    daisyui
+  ]
 }
 
