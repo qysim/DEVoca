8a4217fea10195a121c06d29f3112dd3896c821c
cheesecat47 <cheesecat47@gmail.com>
2024-02-08 04:41:33 +0000

    [FE] design: 카드 컴포넌트 디자인 수정 (#99)
    - WordComponent에 `word` 이름으로 단어 정보 객체 전달 필요.

--- DEVoca-frontend/src/components/word/WordComponent.vue
+++ DEVoca-frontend/src/components/word/WordComponent.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="card card-compact bg-base-100 dark:bg-base-100 shadow-xl mb-4 p-2">
     <div class="card-body">
-      <h2 class="card-title text-base">Hypervisor</h2>
-      <span class="text-gray-400 text-xs">하이퍼바이저</span>
+      <h2 class="card-title text-base">{{ word['wordNameEn'] }}</h2>
+      <span class="text-gray-400 text-xs">{{ word['wordNameKr'] }}</span>
       <hr>
-      <p class="text-sm">가상머신을 실행하기 위한 프로그램</p>
+      <p class="text-sm">{{ word['wordSumm'] }}</p>
       <div class="flex justify-start gap-1 mt-2 text-xs">
-        <span v-for="category in categories" :key="category.id">#{{ category }}</span>
+        <span v-for="category in word['wordCategory']" :key="category.id">#{{ category }}</span>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-
+defineProps({
+  word: Object
+})
 </script>
