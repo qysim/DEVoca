6d45491a3ebdcec19e4c7bc2cb5e8d473d6b98ea
se03013 <se03013@naver.com>
2024-02-08 14:53:17 +0900

    DM & 게시판 화면 구현

--- DEVoca-frontend/src/components/navbar/TopNavbarComponent.vue
+++ DEVoca-frontend/src/components/navbar/TopNavbarComponent.vue
@@ -8,6 +8,9 @@
 
     <!-- 알림페이지 연결 후 swap 기능 구현 예정 -->
     <div class="navbar-end">
+      <RouterLink to="/DMlist">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-7 h-7"><path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" /></svg>
+      </RouterLink>
       <RouterLink to="/alarmpage">
         <button class="btn btn-ghost btn-circle">
           <div class="indicator text-neutral-900">
