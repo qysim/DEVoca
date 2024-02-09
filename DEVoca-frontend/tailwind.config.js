af49623526dbe8ba34b1b87d120ab55c864329d4
신주용 <cheesecat47@gmail.com>
2024-02-09 16:16:23 +0900

    Update tailwind.config.js

--- DEVoca-frontend/tailwind.config.js
+++ DEVoca-frontend/tailwind.config.js
@@ -10,16 +10,19 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans : ['GmarketSansMedium', ...defaultTheme.fontFamily.sans],
+        sans: ['GmarketSansMedium', ...defaultTheme.fontFamily.sans],
         jalnan: ['JalnanGothic'],
       },
       colors: {
-        devoca_skyblue : '#ECFCFF', // 연하늘
+        devoca_skyblue: '#ECFCFF', // 연하늘
         devoca: '#00B4D8', // 대표색
         devoca_sky: '#DDF7FC',
       }
     },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["cupcake"]
+  }
 }
 
