2617f51061a3ca87eb933a57ed1cca595d0a1ba3
cheesecat47 <cheesecat47@gmail.com>
2024-02-09 06:55:46 +0000

    [FE] fix: 의견 카드 정보 추가 (#100)
    - 의견 카드 조회 API로 받아온 데이터 추가
    - Word 카드가 의견 카드에 들어갈 수 있음 -> 이 때 catetory 정보는 생략
      - word 카드 재사용하고, category 정보가 없으면 렌더링 하지 않도록 수정

--- DEVoca-frontend/src/components/word/WordComponent.vue
+++ DEVoca-frontend/src/components/word/WordComponent.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="card card-compact bg-base-100 dark:bg-base-100 shadow-xl mb-4 p-2">
     <div class="card-body">
-      <h2 class="card-title text-base">{{ word['wordNameEn'] }}</h2>
-      <span class="text-gray-400 text-xs">{{ word['wordNameKr'] }}</span>
+      <h2 class="card-title text-base">{{ word.wordNameEn }}</h2>
+      <span class="text-gray-400 text-xs">{{ word.wordNameKr }}</span>
       <hr>
-      <p class="text-sm">{{ word['wordSumm'] }}</p>
-      <div class="flex justify-start gap-1 mt-2 text-xs">
-        <span v-for="category in word['wordCategory']" :key="category.id">#{{ category }}</span>
+      <p class="text-sm">{{ word.wordSumm }}</p>
+      <div v-if="word.wordCategory !== undefined" class="flex justify-start gap-1 mt-2 text-xs">
+        <span v-for="category in word.wordCategory" :key="category.id">#{{ category }}</span>
       </div>
     </div>
   </div>
