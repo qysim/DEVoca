a56e045d478330fe6af5137042aca224f501803c
gysim <qysim@naver.com>
2024-02-09 15:43:47 +0900

    [FE] feat: word 기능 구현 (#92)
    - wordListView 기능 구현
    - wordDetailView 기능 구현

--- DEVoca-frontend/src/stores/word.js
+++ DEVoca-frontend/src/stores/word.js
@@ -3,6 +3,6 @@ import { defineStore } from 'pinia'
 
 export const useWordStore = defineStore('word', () => {
   const wordList = ref({})
-
+  
   return { wordList }
-},{persist:true})
+}, {persist: true})
--- DEVoca-frontend/src/views/word/WordDetailView.vue
+++ DEVoca-frontend/src/views/word/WordDetailView.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-    <div class="bg-neutral-100 py-1">
+    <div class="bg-neutral-100 pt-2">
       <WordComponent v-if="word" :word="word"/>
+      <div v-else class="flex justify-center">
+        <span class="loading loading-dots loading-lg"></span>
+      </div>
     </div>
 
     <!-- 기사 -->
-    <p class="font-jalnan text-xl ml-4 mt-4">관련 최신 기사</p>
+    <p class="font-jalnan text-lg ml-4 mt-4">관련 최신 기사</p>
     <div v-for="news in newsList">
-      <div class="card bg-base-100 shadow-xl m-2" @click="goNews(news.link)">
-        <div class="card-body">
-          <h2 class="card-title">{{ news.title }}</h2>
-          <p v-html="news.description"></p>
+      <div class="card bg-base-100 shadow-xl m-2 text-xs" @click="goNews(news.link)">
+        <div class="card-body p-4">
+          <h2 class="card-title text-sm">{{ news.title }}</h2>
+          <p v-html="news.description" class="truncate"></p>
           <p>{{ changeDateFormat(news.pubDate) }}</p>
         </div>
       </div>
@@ -18,11 +21,9 @@
 
     <div class="collapse collapse-arrow bg-base-100 shadow-xl w-auto">
       <input type="checkbox" /> 
-      <div class="collapse-title text-xl font-medium font-jalnan">
-        카드
-      </div>
-      <div class="collapse-content p-0"> 
-        <CardComponent />
+      <div class="collapse-title text-xl font-medium font-jalnan">관련 카드</div>
+      <div v-for="card in cardList" class="collapse-content p-0"> 
+        <CardComponent :card="card"/>
       </div>
     </div>
   </div>
@@ -43,9 +44,10 @@ const newsList = ref(null)
 const cardList = ref(null)
 const scrollNo = ref(0)
 
-onBeforeMount(async () => {
+onBeforeMount(async() => {
   try {
-    await getWordDetail(props.id, (res) => {
+    await 
+    getWordDetail(props.id, (res) => {
     word.value = res.data.wordDTO
     newsList.value = res.data.newsList
   }, (err) => {
@@ -53,14 +55,14 @@ onBeforeMount(async () => {
   })
 
   getCardListByWord(`${word.value.wordId}/${scrollNo.value}`, (res) => {
-    console.log(res)
+    cardList.value = res.data
+    scrollNo.value++
   }, (err) => {
     console.log(err)
   })
   } catch (err) {
     console.log(err)
   }
-  
 })
 
 const goNews = function (link) {
@@ -69,11 +71,9 @@ const goNews = function (link) {
 
 const changeDateFormat = function (string) {
   const date = new Date(string)
-  const newDate = date.toLocaleDateString('kr-kr')
-  // console.log(newDate)
+  const newDate = date.toLocaleDateString('ko-KR')
   return newDate
 }
-
 
 </script>
 
