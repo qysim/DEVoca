de76db3604723eef1d23a65376b3dd4de0828010
gysim <qysim@naver.com>
2024-02-09 19:19:19 +0900

    [FE] feat: api/card.js 생성 (#104)

---
+++ DEVoca-frontend/src/api/card.js
@@ -0,0 +1,12 @@
+import { localAxios } from '@/util/http-commons'
+
+const local = localAxios()
+
+// export function registCard(data, success, fail) {
+//   local.post(`/card`, data).then(success).catch(fail)
+// }
+
+export function getCardList(param, success, fail) {
+  local.defaults.headers["Authorization"] = sessionStorage.getItem("accessToken")
+  local.get(`/card/list/${param}`).then(success).catch(fail)
+}
