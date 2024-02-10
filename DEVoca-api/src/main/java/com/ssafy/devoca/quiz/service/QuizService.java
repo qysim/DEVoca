3149c2c1cde70125a10a567996688f849ee815ac
amysia <amysia@knu.ac.kr>
2024-02-11 03:42:27 +0900

    [BE] Feat: 대결 퀴즈 생성 및 조회 기능
    - 변경 사항 1 : 대결 도전장 보내기 기능 : 대결에 사용할 단어장 목록 조회
    - 변경 사항 2 : 대결 퀴즈 생성 기능
    - 변경 사항 3 : 대결 퀴즈 조회 기능
    
    - 참고 사항 1 : 대결 퀴즈 DM 전송 기능은 이후 DB 수정이 필요해 보류

--- DEVoca-api/src/main/java/com/ssafy/devoca/quiz/service/QuizService.java
+++ DEVoca-api/src/main/java/com/ssafy/devoca/quiz/service/QuizService.java
@@ -1,8 +1,6 @@
 package com.ssafy.devoca.quiz.service;
 
-import com.ssafy.devoca.quiz.model.QuizDTO;
-import com.ssafy.devoca.quiz.model.QuizListDTO;
-import com.ssafy.devoca.quiz.model.QuizResultDTO;
+import com.ssafy.devoca.quiz.model.*;
 
 import java.util.List;
 
@@ -10,7 +8,6 @@ public interface QuizService {
     public int createQuiz() throws Exception;
 
     public List<QuizDTO> getQuiz() throws Exception;
-
     public void saveQuizResult(QuizResultDTO quizResultDTO) throws Exception;
 
     public int getQuizCnt(int loginUserIdx) throws Exception;
@@ -18,4 +15,11 @@ public interface QuizService {
     public List<QuizListDTO> getQuizResultList(int loginUserIdx) throws Exception;
 
     public List<QuizResultDTO> getQuizResultDetail(int loginUserIdx, int quizId) throws Exception;
+
+    public List<QuizVocaDTO> getBattleVocaList(int loginUserIdx, int oppoUserIdx) throws Exception;
+
+    public int createBattle(BattleRequestDTO battleRequestDTO) throws Exception;
+
+    public List<QuizDTO> getBattleQuiz(int quizId) throws Exception;
+
 }
