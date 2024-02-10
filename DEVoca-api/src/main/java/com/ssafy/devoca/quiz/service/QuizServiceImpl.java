dfc5929c43aeeff558a53d2e19b43c0a919a1b52
amysia <amysia@knu.ac.kr>
2024-02-11 00:15:32 +0900

    [BE] Feat: 퀴즈 결과 조회 기능 구현
    - 변경 사항 : transactional 추가

--- DEVoca-api/src/main/java/com/ssafy/devoca/quiz/service/QuizServiceImpl.java
+++ DEVoca-api/src/main/java/com/ssafy/devoca/quiz/service/QuizServiceImpl.java
@@ -7,12 +7,14 @@ import com.ssafy.devoca.quiz.model.mapper.QuizMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 @Slf4j
+@Transactional
 @RequiredArgsConstructor
 public class QuizServiceImpl implements QuizService{
 
