fdaa305830cb5db821a6d8c2c46c714425e8f275
kimjuyi <juyi077@naver.com>
2024-02-10 19:46:44 +0900

    [BE] feat: DM Redis 저장 및 DB 저장
    - 유저 입장 시 Redis에 저장
    - 실시간 채팅 내역 Redis에 저장
    - 유저 퇴장 시
      Redis에 있는 채팅 내역 DB에 저장 후 Redis 삭제
      Redis에 저장된 유저 삭제
      해당 방에 남아 있는 유저가 없으면 푸시 알림(구현 필요)
      마지막 조회 시간 업데이트

--- DEVoca-api/src/main/java/com/ssafy/devoca/dm/model/mapper/DmMapper.java
+++ DEVoca-api/src/main/java/com/ssafy/devoca/dm/model/mapper/DmMapper.java
@@ -2,6 +2,7 @@ package com.ssafy.devoca.dm.model.mapper;
 
 import com.ssafy.devoca.dm.model.DmDTO;
 import com.ssafy.devoca.dm.model.DmRoomDTO;
+import com.ssafy.devoca.dm.model.LastDateDTO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -21,4 +22,8 @@ public interface DmMapper {
     public int createRoom(@Param("roomUuid") String roomUuid) throws SQLException;
 
     public void insertParticipants(@Param("roomUuid") String roomUuid, @Param("loginUserIdx") int loginUserIdx, @Param("chatUserIdx") int chatUserIdx) throws SQLException;
+    public int getRoomIdxByRoomUuid(String roomUuid) throws SQLException;
+    public String getRoomUuidByRoomIdx(int roomIdx) throws SQLException;
+    public void saveMessages(List<DmDTO> dmDTOList) throws SQLException;
+    public void updateLastDate(LastDateDTO lastDateDTO) throws SQLException;
 }
