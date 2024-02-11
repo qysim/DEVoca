2ce034b98406dc73f70c7e0634f030075ae5af7a
Ryujy <bamdal124@gmail.com>
2024-02-11 16:24:22 +0900

    [BE] feat: 로그인 로직 수정 (#30)

--- DEVoca-api/src/main/java/com/ssafy/devoca/user/controller/UserController.java
+++ DEVoca-api/src/main/java/com/ssafy/devoca/user/controller/UserController.java
@@ -46,7 +46,7 @@ public class UserController {
      * @Github https://github.com/Ryujy
      * */
     @GetMapping("")
-    public ResponseEntity<UserDTO> getUserInfo(@RequestHeader("access-token") String token){
+    public ResponseEntity<UserDTO> getUserInfo(@RequestHeader("token") String token){
         log.info("회원 정보 조회 호출 : {}", token);
         try{
             int userIdx = userService.loadUserIdx(token);
@@ -60,11 +60,11 @@ public class UserController {
     }
 
     @PatchMapping("")
-    public ResponseEntity<UserDTO> updateUserInfo(@RequestHeader("userId") String userId,
+    public ResponseEntity<UserDTO> updateUserInfo(@RequestHeader("token") String token,
                                                   @RequestBody UserDTO userDTO){
         log.info("user 정보 수정 호출");
         try{
-            int userIdx = userService.loadUserIdx(userId);
+            int userIdx = userService.loadUserIdx(token);
             userDTO.setUserIdx(userIdx);
             userService.updateUserInfo(userDTO);
             return ResponseEntity.status(HttpStatus.OK).build();
@@ -78,12 +78,11 @@ public class UserController {
         - delete 후 insert
     */
     @PutMapping("/fav")
-    public ResponseEntity<String> updateFavCategory(@RequestHeader("userId") String userId,
+    public ResponseEntity<String> updateFavCategory(@RequestHeader("token") String token,
                                                  @RequestBody List<Integer> favList){
         log.info("user 관심 분야 설정 호출");
         try{
-//            int userIdx = userService.loadUserIdx(userId);
-            int userIdx = 9;
+            int userIdx = userService.loadUserIdx(token);
             Map<String, Object> params = new HashMap<>();
             params.put("userIdx", userIdx);
             params.put("favList", favList);
@@ -96,10 +95,10 @@ public class UserController {
     }
     
     @GetMapping("/fav")
-    public ResponseEntity<List<FavCategoryDTO>> getFavCategory(@RequestHeader("userId") String userId){
+    public ResponseEntity<List<FavCategoryDTO>> getFavCategory(@RequestHeader("token") String token){
         log.info("회원 관심 분야 조회 호출");
         try{
-            int userIdx = userService.loadUserIdx(userId);
+            int userIdx = userService.loadUserIdx(token);
             List<FavCategoryDTO> favList = userService.getFavCategory(userIdx);
             return ResponseEntity.status(HttpStatus.OK).body(favList);
         } catch (Exception e){
@@ -109,10 +108,10 @@ public class UserController {
     }
 
     @GetMapping("/badge")
-    public ResponseEntity<List<BadgeDTO>> getUserBadges(@RequestHeader("userId") String userId) {
+    public ResponseEntity<List<BadgeDTO>> getUserBadges(@RequestHeader("token") String token) {
         log.info("회원의 배지 목록 조회 호출");
         try{
-            int userIdx = userService.loadUserIdx(userId);
+            int userIdx = userService.loadUserIdx(token);
             List<BadgeDTO> badgeList = userService.getUserBadges(userIdx);
             return ResponseEntity.status(HttpStatus.OK).body(badgeList);
         } catch (Exception e){
@@ -122,10 +121,10 @@ public class UserController {
     }
 
     @GetMapping("/follower")
-    public ResponseEntity<List<UserDTO>> getFollowList(@RequestHeader("userId") String userId){
+    public ResponseEntity<List<UserDTO>> getFollowList(@RequestHeader("token") String token){
         log.info("회원의 팔로워 목록 조회 호출 : 나를 팔로우하는 사람들");
         try{
-            int userIdx = userService.loadUserIdx(userId);
+            int userIdx = userService.loadUserIdx(token);
             List<UserDTO> followerList = userService.getFollowList(userIdx);
             return ResponseEntity.status(HttpStatus.OK).body(followerList);
         } catch (Exception e){
@@ -135,10 +134,10 @@ public class UserController {
     }
 
     @GetMapping("/following")
-    public ResponseEntity<List<UserDTO>> getFollowingList(@RequestHeader("userId") String userId){
+    public ResponseEntity<List<UserDTO>> getFollowingList(@RequestHeader("token") String token){
         log.info("회원의 팔로잉 목록 조회 호출 : 내가 팔로우 하는 사람들");
         try{
-            int userIdx = userService.loadUserIdx(userId);
+            int userIdx = userService.loadUserIdx(token);
             List<UserDTO> followingList = userService.getFollowingList(userIdx);
             return ResponseEntity.status(HttpStatus.OK).body(followingList);
         } catch (Exception e){
@@ -148,10 +147,10 @@ public class UserController {
     }
 
     @GetMapping("/recommend")
-    public ResponseEntity<List<UserDTO>> recommendFollow(@RequestHeader("userId") String userId){
+    public ResponseEntity<List<UserDTO>> recommendFollow(@RequestHeader("token") String token){
         log.info("팔로우 추천 호출");
         try{
-            int userIdx = userService.loadUserIdx(userId);
+            int userIdx = userService.loadUserIdx(token);
             List<UserDTO> recommendList = userService.recommendFollow(userIdx);
             return ResponseEntity.status(HttpStatus.OK).body(recommendList);
         } catch(Exception e){
