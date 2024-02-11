2edb3f4e64a83425c76c203d73c9db2ecafb5112
kimjuyi <juyi077@naver.com>
2024-02-11 21:06:51 +0900

    [FE] fix: kakao login info 수정
    - kakao login info 수정

--- DEVoca-frontend/src/stores/user.js
+++ DEVoca-frontend/src/stores/user.js
@@ -3,7 +3,11 @@ import { defineStore } from "pinia"
 
 export const useUserStore = defineStore("user", () => {
   const isLogin = ref(false)
-  const kakaoUserInfo = ref(null)
+  const kakaoUserInfo = ref({
+    id: 3332004001,
+    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MDc2NDE0NzQsInVzZXJJZCI6IjMzMzIwMDQwMDEifQ.FsJA1AHoAQ6tDzCjV8Sps5aPYn2H3FGzQ21_Qau7-lI',
+    userYn: true
+  })
   const userInfo = ref({
     userId: null,
     userName: null,
