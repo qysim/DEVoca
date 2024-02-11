eb9aa0949b33300097bd3a3cb0eef4338e217006
cheesecat47 <cheesecat47@gmail.com>
2024-02-11 22:39:04 +0900

    [FE] fix: 토큰 추가, 값 존재 확인 (#119)

--- DEVoca-frontend/src/api/word.js
+++ DEVoca-frontend/src/api/word.js
@@ -6,13 +6,13 @@ export function getWordList(param, success, fail) {
   local.get(`/dictionary/${param}`).then(success).catch(fail)
 }
 
-export async function getWordDetail(param, success, fail) {
-  await local.get(`/dictionary/detail/${param}`).then(success).catch(fail)
+export async function getWordDetail(param, config, success, fail) {
+  await local.get(`/dictionary/detail/${param}`, config).then(success).catch(fail)
 }
 
-export function getCardListByWord(param, success, fail) {
+export function getCardListByWord(param, config, success, fail) {
   // param = {wordId}/{scroll}
-  local.get(`/dictionary/detail/${param}`).then(success).catch(fail)
+  local.get(`/dictionary/detail/${param}`, config).then(success).catch(fail)
 }
 
 // author: cheesecat47 <cheesecat47@gmail.com>
--- DEVoca-frontend/src/views/word/WordDetailView.vue
+++ DEVoca-frontend/src/views/word/WordDetailView.vue
@@ -11,12 +11,12 @@
     <div class="collapse collapse-arrow bg-base-100 shadow-xl w-full mb-4">
       <input type="checkbox" />
       <div class="collapse-title text-xl font-medium font-jalnan">관련 기사</div>
-      <div v-if="newsList.length === 0" class="collapse-content p-0">
+      <div v-if="newsList && newsList.length === 0" class="collapse-content p-0">
         <div class="card card-compact bg-base-100 dark:bg-base-100 shadow-xl mb-4 p-2">
           <div class="card-body">관련 기사가 없습니다!</div>
         </div>
       </div>
-      <div v-if="newsList.length > 0" class="collapse-content p-0">
+      <div v-if="newsList && newsList.length > 0" class="collapse-content p-0">
         <NewsComponent v-for="news in newsList" :key="news.id" :news="news" />
       </div>
     </div>
@@ -24,12 +24,12 @@
     <div class="collapse collapse-arrow bg-base-100 shadow-xl w-full">
       <input type="checkbox" />
       <div class="collapse-title text-xl font-medium font-jalnan">관련 카드</div>
-      <div v-if="cardList.length === 0" class="collapse-content p-0">
+      <div v-if="cardList && cardList.length === 0" class="collapse-content p-0">
         <div class="card card-compact bg-base-100 dark:bg-base-100 shadow-xl mb-4 p-2">
           <div class="card-body">관련 카드가 없습니다!</div>
         </div>
       </div>
-      <div v-if="cardList.length > 0" class="collapse-content p-0">
+      <div v-if="cardList && cardList.length > 0" class="collapse-content p-0">
         <CardComponent v-for="card in cardList" :key="card.id" :card="card" />
       </div>
     </div>
@@ -52,18 +52,24 @@ const newsList = ref(null)
 const cardList = ref(null)
 const scrollNo = ref(0)
 
+// TODO: 토큰 제거 필요
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhY2Nlc3MtdG9rZW4iLCJpYXQiOjE3MDc2NDE0NzQsInVzZXJJZCI6IjMzMzIwMDQwMDEifQ.FsJA1AHoAQ6tDzCjV8Sps5aPYn2H3FGzQ21_Qau7-lI'
+
 onBeforeMount(async () => {
   try {
-    await
-      getWordDetail(props.id, (res) => {
-        word.value = res.data.wordDTO
-        newsList.value = res.data.newsList
+    await getWordDetail(props.id, {
+      headers: { token: token }
+    }, (res) => {
+      word.value = res.data.wordDTO
+      newsList.value = res.data.newsList
+
+      getCardListByWord(`${word.value.wordId}/${scrollNo.value}`, {
+        headers: { token: token }
+      }, (res) => {
+        cardList.value = res.data
       }, (err) => {
         console.log(err)
       })
-
-    getCardListByWord(`${word.value.wordId}/${scrollNo.value}`, (res) => {
-      cardList.value = res.data
     }, (err) => {
       console.log(err)
     })
