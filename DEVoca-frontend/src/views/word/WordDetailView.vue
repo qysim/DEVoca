cc54300ddf389e15d0622b8380331b3cd09cbdaf
cheesecat47 <cheesecat47@gmail.com>
2024-02-11 19:11:08 +0900

    [FE] feat: 관련 카드 추가 (#119)

--- DEVoca-frontend/src/views/word/WordDetailView.vue
+++ DEVoca-frontend/src/views/word/WordDetailView.vue
@@ -10,7 +10,7 @@
     <!-- 기사 -->
     <div class="collapse collapse-arrow bg-base-100 shadow-xl w-full mb-4">
       <input type="checkbox" />
-      <div class="collapse-title text-xl font-medium font-jalnan">관련 최신 기사</div>
+      <div class="collapse-title text-xl font-medium font-jalnan">관련 기사</div>
       <div v-if="newsList.length === 0" class="collapse-content p-0">
         <div class="card card-compact bg-base-100 dark:bg-base-100 shadow-xl mb-4 p-2">
           <div class="card-body">관련 기사가 없습니다!</div>
@@ -24,8 +24,13 @@
     <div class="collapse collapse-arrow bg-base-100 shadow-xl w-full">
       <input type="checkbox" />
       <div class="collapse-title text-xl font-medium font-jalnan">관련 카드</div>
-      <div v-for="card in cardList" class="collapse-content p-0">
-        <CardComponent :card="card" />
+      <div v-if="cardList.length === 0" class="collapse-content p-0">
+        <div class="card card-compact bg-base-100 dark:bg-base-100 shadow-xl mb-4 p-2">
+          <div class="card-body">관련 카드가 없습니다!</div>
+        </div>
+      </div>
+      <div v-if="cardList.length > 0" class="collapse-content p-0">
+        <CardComponent v-for="card in cardList" :key="card.id" :card="card" />
       </div>
     </div>
   </div>
@@ -59,7 +64,6 @@ onBeforeMount(async () => {
 
     getCardListByWord(`${word.value.wordId}/${scrollNo.value}`, (res) => {
       cardList.value = res.data
-      scrollNo.value++
     }, (err) => {
       console.log(err)
     })
