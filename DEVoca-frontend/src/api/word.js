94727170dfe813f3e40e75dfcea598f991eeaf24
cheesecat47 <cheesecat47@gmail.com>
2024-02-11 23:07:08 +0900

    [FE] fix: 토큰 사용 수정 (#119)

--- DEVoca-frontend/src/api/word.js
+++ DEVoca-frontend/src/api/word.js
@@ -1,20 +1,25 @@
 import { localAxios } from '@/util/http-commons'
 
 const local = localAxios()
-const localUserInfo = JSON.parse(localStorage.getItem('user'))
-const accessToken = localUserInfo.kakaoUserInfo.token
-local.defaults.headers['token'] = accessToken
 
 export function getWordList(param, success, fail) {
   local.get(`/dictionary/${param}`).then(success).catch(fail)
 }
 
 export async function getWordDetail(param, success, fail) {
-  await local.get(`/dictionary/detail/${param}`).then(success).catch(fail)
+  await local.get(`/dictionary/detail/${param}`, {
+    headers: {
+      token: JSON.parse(localStorage.getItem('user')).kakaoUserInfo.token
+    }
+  }).then(success).catch(fail)
 }
 
 export function getCardListByWord(param, success, fail) {
-  local.get(`/dictionary/detail/${param}`).then(success).catch(fail)
+  local.get(`/dictionary/detail/${param}`, {
+    headers: {
+      token: JSON.parse(localStorage.getItem('user')).kakaoUserInfo.token
+    }
+  }).then(success).catch(fail)
 }
 
 // author: cheesecat47 <cheesecat47@gmail.com>
