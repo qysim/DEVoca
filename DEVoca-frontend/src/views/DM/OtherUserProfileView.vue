4dc7ffebcdde37b05d3da834ac00f5436c64f038
cheesecat47 <cheesecat47@gmail.com>
2024-02-13 12:26:06 +0000

    [FE] fix: 다른 유저 페이지 유저 정보를 ProfileCardComponent로 통합 (#166)

--- DEVoca-frontend/src/views/DM/OtherUserProfileView.vue
+++ DEVoca-frontend/src/views/DM/OtherUserProfileView.vue
@@ -1,49 +1,23 @@
 <template>
-  <div class="card card-side bg-devoca_skyblue">
-    <div class="avatar">
-      <div class="w-24 h-24 rounded-full my-10 ml-5">
-        <img src="https://daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg" />
-      </div>
-    </div>
-    <div class="card-body">
-      <div class="flex flex-row justify-between">
-        <h2 class="card-title mt-6">닉네임 위치</h2>
-        <div class="flex justify-start">
-        </div>
-      </div>
-      <p class="text-sm">한 줄 소개자리
-      </p>
-      <div class="flex-row">
-        <a href="" class="mr-5 text-sm">팔로우11</a>
-        <a href="" class="text-sm">팔로워11</a>
-      </div>
-      <div class="flex justify-between ">
-        <div>
-          <a href="" class="ml-1">총 업적 00개</a>
-        </div>
-        <div>
-          <p>등급</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <ProfIleCardComponents :otherUserId="userId" />
   <div class="flex justify-between mt-3">
     <div>
       <button type="submit" class="btn btn-sm w-24 ml-5 bg-devoca text-white">팔로우</button>
-    </div> 
+    </div>
     <div>
       <button type="submit" class="btn btn-sm w-24 bg-devoca text-white">DM</button>
-    </div> 
+    </div>
     <div>
       <button type="submit" class="btn btn-sm w-24 mr-5 bg-devoca text-white">VS</button>
-    </div>  
-    
     </div>
-    
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import ProfIleCardComponents from '@/components/mypage/ProfIleCardComponents.vue'
+import { useRoute } from "vue-router";
 
+const route = useRoute()
+const userId = ref(route.params.id)
 </script>
-
-<style scoped></style>
