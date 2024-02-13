65d4d0c12aa4ba70ed081e6f04f0bca5229b5606
kimjuyi <juyi077@naver.com>
2024-02-13 22:31:14 +0900

    [BE] feat: Battle Quiz와 DM 기능 연결 (#162)
    - Battle Quiz 제출 시 응전자에게 DM 메시지 보내기 기능 구현
    - 푸시 알림 기능 연결

--- DEVoca-api/src/main/java/com/ssafy/devoca/quiz/controller/QuizController.java
+++ DEVoca-api/src/main/java/com/ssafy/devoca/quiz/controller/QuizController.java
@@ -1,5 +1,7 @@
 package com.ssafy.devoca.quiz.controller;
 
+import com.ssafy.devoca.dm.model.DmDTO;
+import com.ssafy.devoca.dm.service.DmService;
 import com.ssafy.devoca.notify.service.NotifyService;
 import com.ssafy.devoca.quiz.model.*;
 import com.ssafy.devoca.quiz.service.QuizService;
@@ -11,7 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +26,7 @@ public class QuizController {
     private final QuizService quizService;
     private final NotifyService notifyService;
     private final UserService userService;
+    private final DmService dmService;
 
     // 스케줄러 설정 : 월~금 오전 8시 35분에 퀴즈 생성 및 저장 메서드 실행
     @Scheduled(cron = "0 35 8 ? * MON-FRI", zone = "Asia/Seoul")
@@ -163,6 +168,49 @@ public class QuizController {
             int userIdx = userService.loadUserIdxById(userId);
             quizResultDTO.setUserIdx(userIdx);
             quizService.saveBattleResult(quizResultDTO);
+
+            // dm 저장을 위한 정보 가져오기
+            int battleUserIdx = quizService.getBattleUserIdx(userIdx, quizResultDTO.getQuizId());
+            String roomUuid = dmService.getRoomUuid(userIdx, battleUserIdx);
+
+            // 두 사용자가 참여한 채팅방이 없을 경우 새로운 채팅방 생성
+            if(roomUuid == null) {
+                UUID uuid = UUID.randomUUID();
+                roomUuid = uuid.toString().substring(0, 8);
+
+                dmService.createRoom(roomUuid);
+                log.info("방 생성 완료");
+
+                dmService.insertParticipants(roomUuid, userIdx, battleUserIdx);
+                log.info("참여자 저장 완료");
+            }
+
+            int roomIdx = dmService.getRoomIdxByRoomUuid(roomUuid);
+
+            String userNickName = userService.getUserNickName(userIdx);
+
+            // dm 내용 설정
+            DmDTO dmDTO = new DmDTO();
+            dmDTO.setRoomIdx(roomIdx);
+            dmDTO.setSendUserId(userId);
+            dmDTO.setDmContent(userNickName + "님이 도전장을 보내셨습니다!");
+            dmDTO.setDmSendDate(new Timestamp(System.currentTimeMillis()));
+            dmDTO.setDmBattleQuizId(quizResultDTO.getQuizId());
+
+            // dm db 저장
+            dmService.saveMessage(dmDTO);
+
+            try {
+                log.info("updateLastDateSendDate");
+                dmService.updateLastDateSendDate(dmDTO);
+            } catch (Exception e) {
+                log.error("updateLastDateSendDate 마지막 조회 시간 = 메시지 전송 시간으로 저장 에러 : {}", e);
+                throw new RuntimeException(e);
+            }
+
+            // 푸시 알림 보내기
+            notifyService.send(battleUserIdx, 4, roomIdx);
+
             return ResponseEntity.status(HttpStatus.OK).build();
         }catch (Exception e){
             log.error("퀴즈 결과 저장 실패 : {}", e);
