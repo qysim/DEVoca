f4315f622684dd411544cdf6e56cf616b77d4081
cheesecat47 <cheesecat47@gmail.com>
2024-02-13 12:51:26 +0000

    [FE] feat: 팔로우 버튼 API 연결 (#166)

--- DEVoca-frontend/src/views/DM/OtherUserProfileView.vue
+++ DEVoca-frontend/src/views/DM/OtherUserProfileView.vue
@@ -2,22 +2,46 @@
   <ProfIleCardComponents :otherUserId="userId" />
   <div class="flex justify-between mt-3">
     <div>
-      <button type="submit" class="btn btn-sm w-24 ml-5 bg-devoca text-white">팔로우</button>
+      <button :key="userInfo.followingYn" v-if="userInfo.followingYn === 'N'"
+        class="btn btn-sm w-24 ml-5 bg-devoca text-white"
+        @click="followHandler(userInfo.followingYn, userInfo.userId)">팔로우</button>
+      <button v-else class="btn btn-sm w-24 ml-5 border-devoca text-devoca"
+        @click="followHandler(userInfo.followingYn, userInfo.userId)">팔로우</button>
     </div>
     <div>
-      <button type="submit" class="btn btn-sm w-24 bg-devoca text-white">DM</button>
+      <button class="btn btn-sm w-24 bg-devoca text-white">DM</button>
     </div>
     <div>
-      <button type="submit" class="btn btn-sm w-24 mr-5 bg-devoca text-white">VS</button>
+      <button class="btn btn-sm w-24 mr-5 bg-devoca text-white">VS</button>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onBeforeMount } from 'vue'
+import { useRoute } from 'vue-router'
+import { getOtherUserInfo } from '@/api/user.js'
+import { followUser, unfollowUser } from "@/api/mypage"
 import ProfIleCardComponents from '@/components/mypage/ProfIleCardComponents.vue'
-import { useRoute } from "vue-router";
 
 const route = useRoute()
+
 const userId = ref(route.params.id)
+const userInfo = ref({})
+
+onBeforeMount(() => {
+  getOtherUserInfo(userId.value, (res) => {
+    userInfo.value = res.data
+  }, null)
+})
+
+const followHandler = (option, id) => {
+  if (option === 'N') {
+    followUser(id)
+    userInfo.value.followingYn = 'Y'
+  } else {
+    unfollowUser(id)
+    userInfo.value.followingYn = 'N'
+  }
+}
 </script>
