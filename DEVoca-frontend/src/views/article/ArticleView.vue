0049218519c589ade626afcd83fd1a72d51a8f44
se03013 <se03013@naver.com>
2024-02-14 11:16:18 +0900

    게시판 상단 탭 수정`

--- DEVoca-frontend/src/views/article/ArticleView.vue
+++ DEVoca-frontend/src/views/article/ArticleView.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="m-2 h-full flex flex-col">
-    <div role="tablist" class="tabs tabs-bordered tabs-lg">
-      <input :class="{ 'active': boardType === 0 }" @click="changeBoardType(0)" type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="자유" />
-      <input :class="{ 'active': boardType === 1 }" @click="changeBoardType(1)" type="radio" name="my_tabs_1" role="tab" class="tab" aria-label="Q&A" />
+    <div role="tablist" class="tabs tabs-bordered m-6">
+      <input class="tab text-lg"
+      :class="{ 'tab-active': boardType === 0 }" @click="changeBoardType(0)" type="radio" name="my_tabs_1" role="tab" aria-label="자유" />
+      <input class="tab text-lg"
+      :class="{ 'tab-active': boardType === 1 }" @click="changeBoardType(1)" type="radio" name="my_tabs_1" role="tab" aria-label="Q&A" />
     </div>
 
     <ArticleComponent v-for="board in boards" :board="board" :key="board.id" />
@@ -10,7 +12,6 @@
 </template>
 
 <script setup>
-import SearchBarComponent from '@/components/common/SearchBarComponent.vue';
 import ArticleComponent from '@/components/article/ArticleComponent.vue';
 import { ref, onMounted } from 'vue';
 import { getBoardList } from '@/api/board';
@@ -27,7 +28,6 @@ const fetchBoardData = () => {
   getBoardList(boardType.value, 
     (response) => {
       boards.value = response.data;
-      // console.log('게시판 데이터:', response.data);
     },
     (error) => {
       console.error(error);
