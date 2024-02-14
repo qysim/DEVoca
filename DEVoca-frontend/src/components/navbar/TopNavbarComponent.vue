51b276aeb5b1f126681cd2b52771b00860c48d09
se03013 <se03013@naver.com>
2024-02-14 23:31:39 +0900

    TopNavbarComponent 수정

--- DEVoca-frontend/src/components/navbar/TopNavbarComponent.vue
+++ DEVoca-frontend/src/components/navbar/TopNavbarComponent.vue
@@ -9,7 +9,7 @@
     <!-- 알림페이지 연결 후 swap 기능 구현 예정 -->
     <div class="navbar-end">
       <!-- <router-link :to="{name : 'DMListView', params : { id : userId }}"> -->
-        <button @click="goDM(userStore.kakaoUserInfo['id'])">
+        <button @click="goDM(userStore.kakaoUserInfo['id'])" class="btn btn-ghost btn-circle">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" transform="rotate(-30)" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-7s h-7 mb-2"><path stroke-linecap="round" stroke-linejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" /></svg>
         </button>
       <!-- </router-link> -->
