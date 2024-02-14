e5b6756bdb81131690d85cb6e7105a5f6d3f64ed
cheesecat47 <cheesecat47@gmail.com>
2024-02-14 04:55:01 +0000

    [FE] fix: 유저 프로필 부분은 프로필 화면으로 이동, 팔로우 버튼은 버튼 핸들러 실행 (#165)

--- DEVoca-frontend/src/components/common/FollowCardComponent.vue
+++ DEVoca-frontend/src/components/common/FollowCardComponent.vue
@@ -1,17 +1,17 @@
 <template>
-  <router-link :to="{ name: 'OtherUserProfileView', params: { id: userInfo.userId } }">
+  <router-link :to="{ name: 'OtherUserProfileView', params: { id: userInfo.userId } }" custom v-slot="{ href, navigate }">
     <div class="card card-side bg-base-100 items-center py-2 px-4">
-      <div class="avatar basis-1/6">
+      <a :href="href" @click="navigate" class="avatar basis-1/6">
         <div class="rounded-full">
           <img :src="userInfo.userImg" />
         </div>
-      </div>
-      <div class="card-body grow p-2">
+      </a>
+      <a :href="href" @click="navigate" class="card-body grow p-2">
         <div class="flex flex-col">
           <h2 class="card-title text-lg font-jalnan">{{ userInfo.userNickName }}</h2>
           <span class="text-sm">{{ userInfo.userIntro }}</span>
         </div>
-      </div>
+      </a>
       <div class="flex flex-col basis-1/4 text-right">
         <button v-if="userInfo.followingYn === 'N'" class="btn bg-devoca text-white"
           @click="followHandler(userInfo.followingYn, userInfo.userId)">팔로우</button>
