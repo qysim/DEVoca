ff621c6f6c4cc892eebdf88ce6b26a324e6b75b7
amysia <amysia@knu.ac.kr>
2024-02-14 10:33:29 +0900

    [FE] feat: 퀴즈 내역 조회 및 상세 페이지 연결
    - 변경 사항 1 : 퀴즈 내역 조회 페이지, 컴포넌트와 상세 페이지, 컴포넌트 연결
    - 변경 사항 2 : 퀴즈 상세 조회 api 연결
    
    - 참고 사항 : 대결 상세 조회 페이지로 분화, api 연결 필요

--- DEVoca-frontend/src/router/index.js
+++ DEVoca-frontend/src/router/index.js
@@ -223,7 +223,7 @@ const router = createRouter({
           component : QuizListView,
         },
         {
-          path : '/quizdetail',
+          path : '/quizdetail/:quizId',
           name : 'QuizDetailView',
           component : QuizDetailView,
         },
