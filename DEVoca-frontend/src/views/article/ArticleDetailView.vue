abd5e59a8e03d372f7c6cbe5f456d58f4ebeb160
amysia <amysia@knu.ac.kr>
2024-02-16 04:00:39 +0900

    [FE] feat: 게시판 댓글 작성, 삭제 기능
    - 변경 내용 : 게시판 댓글 작성 및 삭제 기능

--- DEVoca-frontend/src/views/article/ArticleDetailView.vue
+++ DEVoca-frontend/src/views/article/ArticleDetailView.vue
@@ -4,8 +4,14 @@
   </div>
   <ArticleDetailComponent :key="boardInfo" :board="boardInfo" />
   <p class="ml-5 mt-5">댓글 {{ comments.length }}개</p>
-  <div v-for="comment in comments" :key="comment.commentId">
-    <CommentComponent :comment="comment" />
+  <div class="mb-14">
+    <div v-for="comment in comments" :key="comment.commentId">
+      <CommentComponent :comment="comment" />
+    </div>
+  </div>
+  <div class="input-container flex mt-4 fixed bottom-16 left-0 right-0 bg-base-100">
+    <input type="text" placeholder="댓글을 남겨보세요" class="mx-2 input input-bordered w-full" v-model="inputComment" @keyup.enter="submitComment"/>
+    <button @click="submitComment" class="btn bg-devoca text-white text-lg">등록</button>
   </div>
 </template>
 
@@ -14,6 +20,7 @@ import { ref, onBeforeMount } from 'vue';
 import { getBoardDetail, getCommentList } from '@/api/board.js'
 import ArticleDetailComponent from "@/components/article/ArticleDetailComponent.vue";
 import CommentComponent from "@/components/article/CommentComponent.vue";
+import { registComment } from "@/api/card";
 
 const props = defineProps({
   boardId: String
@@ -22,6 +29,7 @@ const props = defineProps({
 const userInfo = ref({})
 const boardInfo = ref({})
 const comments = ref([])
+const inputComment = ref(null);
 
 const dateString = ref('')
 const timeString = ref('')
@@ -52,4 +60,26 @@ onBeforeMount(() => {
     comments.value = res.data
   }, null)
 })
+
+
+const submitComment = () => {
+  const commentInfo = {
+    cardBoardId: props.boardId,
+    flag: 'board',
+    commentContent: inputComment.value
+  }
+  registComment(commentInfo, (res) => {
+    inputComment.value = ''
+    updateComments()
+  }, (err) => {
+    console.log(err)
+  })
+}
+
+const updateComments = () => {
+  getCommentList("board", props.boardId, (res) => {
+    comments.value = res.data
+  }, null)
+}
+
 </script>
